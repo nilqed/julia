c076efa761410cc4495e0138f38de32715916fbc
Milan Bouchet-Valat <nalimilan@club.fr>
2017-12-11 21:25:03 +0100

    Remove 0.6 deprecations

--- stdlib/Unicode/src/Unicode.jl
+++ stdlib/Unicode/src/Unicode.jl
@@ -4,21 +4,14 @@ __precompile__(true)
 
 module Unicode
 
-import Base.UTF8proc: normalize_string, graphemes, is_assigned_char, textwidth, isvalid,
-                      islower, isupper, isalpha, isdigit, isxdigit, isnumber, isalnum,
-                      iscntrl, ispunct, isspace, isprint, isgraph,
-                      lowercase, uppercase, titlecase, lcfirst, ucfirst
+using Base.UTF8proc: normalize_string, graphemes, is_assigned_char, textwidth, isvalid,
+                     islower, isupper, isalpha, isdigit, isxdigit, isnumber, isalnum,
+                     iscntrl, ispunct, isspace, isprint, isgraph,
+                     lowercase, uppercase, titlecase, lcfirst, ucfirst
 
 export normalize_string, graphemes, is_assigned_char, textwidth, isvalid,
        islower, isupper, isalpha, isdigit, isxdigit, isnumber, isalnum,
        iscntrl, ispunct, isspace, isprint, isgraph,
        lowercase, uppercase, titlecase, lcfirst, ucfirst
-
-# Julia 0.7 deprecations
-for name in ("alnum", "alpha", "cntrl", "digit", "number", "graph",
-             "lower", "print", "punct", "space", "upper", "xdigit")
-    f = Symbol("is", name)
-    @eval Base.@deprecate ($f)(s::AbstractString) all($f, s)
-end
 
 end
